1ca0200fdf311c20a19695d5e8f4fae3b021d1ae
chanvuu <caochanvu2002@gmail.com>
2026-03-09 21:45:36 +0700

    style: reformat code and remove unused spaces

--- SportBooking.Domain/Entities/BankAccount.cs
+++ SportBooking.Domain/Entities/BankAccount.cs
@@ -7,15 +7,11 @@ namespace SportBooking.Domain.Entities
     public class BankAccount
     {
         public Guid Id { get; set; }
-
         public Guid OwnerId { get; set; }
-
         public string BankCode { get; set; } 
         public string BankName { get; set; }
-
         public string AccountNumber { get; set; }
         public string AccountName { get; set; }
-
         public DateTime CreatedAt { get; set; }
     }
 }
